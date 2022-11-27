54d843b6d7a24dc261b10210df5aa0bdc7c55c71
Tal Karidi-Heller <talkaridiheller@gmail.com>
2022-11-27 17:47:33 +0200

    Fixing rotation

--- src/main/java/frc/robot/subsystems/SwerveDriveTrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveTrain.java
@@ -46,8 +46,8 @@ public class SwerveDriveTrain extends SubsystemBase {
   public void setRelativeSwerveVelocoties(Vector vector, double omega) {
     // Module 1:
     Vector module_1_vector = new Vector(
-      (vector.getX() + omega * (length / 2)), 
-      (vector.getY() - omega * (width / 2)), 
+      (vector.getX() - omega * (length / 2)), 
+      (vector.getY() + omega * (width / 2)), 
       Representation.Cartisian
     );
     this.rightFront.setVector(module_1_vector);
@@ -61,8 +61,8 @@ public class SwerveDriveTrain extends SubsystemBase {
 
     // Module 3:
     Vector module_3_vector = new Vector(
-      (vector.getX() - omega * (length / 2)), 
-      (vector.getY() + omega * (width / 2)), 
+      (vector.getX() + omega * (length / 2)), 
+      (vector.getY() - omega * (width / 2)), 
       Representation.Cartisian
     );
     this.leftRear.setVector(module_3_vector);
