03af6a8ca672bad25c68fdef70f34d938ae96137
Tal Karidi-Heller <talkaridiheller@gmail.com>
2022-11-30 15:22:41 +0200

    updating LogCommand

--- src/main/java/frc/robot/commands/LogCommand.java
+++ src/main/java/frc/robot/commands/LogCommand.java
@@ -7,12 +7,19 @@ package frc.robot.commands;
 import java.io.DataOutputStream;
 import java.util.concurrent.Callable;
 
+import javax.xml.stream.events.ProcessingInstruction;
+
+import com.ctre.phoenix.time.StopWatch;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class LogCommand extends CommandBase {
   int motorId;
   Callable<Double> getValueFunction;
   DataOutputStream outputStream;
+  boolean printed = false;
+  StopWatch stopWatch = new StopWatch();
+  double lastTimeSent = 0;
 
   /** Creates a new LogCommand. */
   public LogCommand(int motorId, Callable<Double> getValueFunction, DataOutputStream outputStream) {
@@ -23,20 +30,30 @@ public class LogCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    stopWatch.start();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute(){
     try {
-      this.outputStream.writeInt(motorId);
-      System.out.println(motorId + ", " + this.getValueFunction.call());
-      this.outputStream.writeDouble(this.getValueFunction.call());
-      this.outputStream.flush();
+      if (stopWatch.getDuration() - lastTimeSent >= 0.05) {
+        this.outputStream.writeInt(motorId);
+        this.outputStream.writeDouble(this.stopWatch.getDuration());
+        this.outputStream.writeDouble(this.getValueFunction.call());
+        this.outputStream.flush();
+        System.out.println(motorId + ", " + this.getValueFunction.call());
+        lastTimeSent = stopWatch.getDuration();
+      }
+      
     }
 
     catch (Exception exception) {
-      // exception.printStackTrace();
+      if (printed == false) {
+        exception.printStackTrace();
+        printed = true;
+      }
     }
 
   }
